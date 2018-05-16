6b3f63ef765090e40549d375be8009479d135009
wanghaov <wanghaov@yonyou.com>
2018-05-16 16:08:15 +0800

    '为数据库中的几条记录添加图片'

---
+++ src/main/java/com/lingling/config/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package com.lingling.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by WangHao on 2018/5/15.
+ */
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedHeaders("*")
+                .allowedMethods("*")
+                .allowedOrigins("*")
+                .allowCredentials(true);
+    }
+}
